5f77f180a1486a3050af916ed848fb6d7ab3b7eb
feras <ferasboulala@gmail.com>
2019-11-29 21:31:37 -0500

    Remove useless comments

--- cfilt/kalman1d.h
+++ cfilt/kalman1d.h
@@ -8,10 +8,16 @@
  * All gaussians are represented by the structure cfilt_gauss (a pair of mean and variance).
  */
 
-// Predicts the state variable x according to the rate of change dx.
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void cfilt_kalman1d_predict(struct cfilt_gauss* x_pred, struct cfilt_gauss x, struct cfilt_gauss dx);
 
-// Updates the state variable x according to the sensor data z.
 void cfilt_kalman1d_update(struct cfilt_gauss* x, struct cfilt_gauss x_pred, struct cfilt_gauss z);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // KALMAN1D_H
