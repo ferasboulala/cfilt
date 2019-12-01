7f7137714728b64b56b20fcbc8c45458f589cdff
feras <ferasboulala@gmail.com>
2019-12-01 11:05:08 -0500

    Add license notice to source files

--- cfilt/gh.c
+++ cfilt/gh.c
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2020 Feras Boulala <ferasboulala@gmail.com>
+ * 
+ * This file is part of cfilt.
+ * 
+ * cfilt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * cfilt is distributed in the hope it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU GEneral Public License
+ * along with cfilt. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "cfilt/gh.h"
 
 #include <gsl/gsl_errno.h>
--- cfilt/kalman.h
+++ cfilt/kalman.h
@@ -1,7 +1,34 @@
+/**
+ * Copyright 2020 Feras Boulala <ferasboulala@gmail.com>
+ * 
+ * This file is part of cfilt.
+ * 
+ * cfilt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * cfilt is distributed in the hope it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU GEneral Public License
+ * along with cfilt. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef KALMAN_H
 #define KALMAN_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void cfilt_kalman_predict(void);
 void cfilt_kalman_update(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // KALMAN_H
